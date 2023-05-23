4074c38e9fa2b48dc78563d36bde0102eab26d84
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-05-23 14:30:11 -0500

    created Vue projects sandbox and daredevil-forum, installs. Added extension Volar. Migrated playground.html to App.vue. Removed CSS from main to allow for original formating to appear

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,47 +1,79 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<!-- our JS -->
+<script>
+export default {
+
+  data() {
+            return {
+              count: 10,
+              counterTitle: "Counter Standard",
+              increaseAmount: 8,
+              // reactive data message. Rendered with curly brace syntax in div above
+              message: "Hello it works!",
+
+              listOfNumbers: [
+                { name: 1, id: "123", list: [1, 2, 3] },
+                { name: 2, id: "456", list: [1, 2, 3] },
+                { name: 3, id: "789", list: [1, 2, 3] },
+                { name: 4, id: "101112", list: [1, 2, 3] },
+                { name: 5, id: "131416", list: [1, 2, 3] },
+              ],
+            };
+          },
+          computed: {
+            displayTitle() {
+              if (this.count > 20) {
+                return "Counter Standard - Very Long";
+              } else {
+                return "Counter Standard";
+              }
+            },
+            optimizedIncreaseAmount() {
+              return this.displayTitle.length * this.increaseAmount;
+            },
+          },
+          methods: {
+            increaseCount(newAmount, event) {
+              // if we did: this.increaseAmount = event.target.value, we would see the counter simply tacking on the "Increase By:" number to the end of the count as it is a string. We could use Number(this.increaseAmount = event.target.value) and it would work, but it it would be a round-about way of doing things. Instead, we can used v-model in our input
+              console.log(newAmount);
+              console.log(event);
+              this.count += this.optimizedIncreaseAmount;
+            },
+          },
+          // watch: {
+          //   count(newValue) {
+          //     if (newValue > 20) {
+          //       this.counterTitle += "Very Long";
+          //     }
+          //   },
+          //
+}
 </script>
 
+<!-- our HTML -->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <h1>{{displayTitle}}</h1>
+      <p :data-increment-by="increaseAmount">{{count}}</p>
+      <button v-on:click="increaseCount">Increase Count</button>
+      <h1>{{increaseAmount}}</h1>
+      
+      <p>{{optimizedIncreaseAmount}}</p>
+      <div>
+        <label for="increaseAmount">Increase By:</label>
+        <!-- telling Vue: on input event, run function changeIncreaseAmount -->
+        <!-- we could use v-bind:value="increaseAmount"
+          v-on:input="changeIncreaseAmount", but v-model configures that under the hood for us -->
+        <input type="number" v-model="increaseAmount" />
+      </div>
+      <hr />
+      <p v-if="message.length % 2 === 0">Even: {{message.toUpperCase()}}</p>
+      <p v-else>Odd: {{message}}</p>
+      <ul v-for="(, index) in listOfNumbers" :key=" 'item-${index}'">
+        <li>
+          {{item.id}}
+          <ul>
+            <li v-for="(, index) in item.list" :key="'number-${index}'">{{number}}</li>
+          </ul>
+        </li>
+      </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
